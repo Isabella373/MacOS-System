d29d192353ad5a2a2883836884598e59757323ed
Isabella373 <99710738+Isabella373@users.noreply.github.com>
2022-10-28 22:32:03 -0700

    Oct

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,10 +2,12 @@ import './index.scss';
 
 import React from 'react';
 
+import Header from '../Header';
 import Main from '../Main';
 
 const App: React.FC = (): JSX.Element => (
     <div className="App">
+        <Header />
         <Main />
     </div>
 );
